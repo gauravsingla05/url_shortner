af80e63b4061ba36c7d2a4dfebb213c1be3b2e88
Gourav singla <gouravsingla05@gmail.com>
2023-07-21 20:28:19 -0500

    Update isValidUrl.js

--- src/utils/isValidUrl.js
+++ src/utils/isValidUrl.js
@@ -1,28 +1,31 @@
 const validateUrl = (url) => {
-    try {
-      const trimmedUrl = url.trim();
+  try {
+    const trimmedUrl = url.trim();
   
-      const fullUrl = !trimmedUrl.startsWith('http://') && !trimmedUrl.startsWith('https://')
-        ? 'http://' + trimmedUrl
-        : trimmedUrl;
+    const fullUrl = !trimmedUrl.startsWith('http://') && !trimmedUrl.startsWith('https://')
+      ? 'http://' + trimmedUrl
+      : trimmedUrl;
   
-   
-      const parsedUrl = new URL(fullUrl);
+    const parsedUrl = new URL(fullUrl);
   
-      const hostname = parsedUrl.hostname;
+    const hostname = parsedUrl.hostname;
   
-      const lastDotIndex = hostname.lastIndexOf('.');
-      if (lastDotIndex === -1 || lastDotIndex >= hostname.length - 3) {
-        return false;
-      }
-  
-    
-      return true;
-    } catch (error) {
+    // Check if the hostname contains at least one dot
+    if (hostname.indexOf('.') === -1) {
       return false;
     }
-  };
   
+    // Check if the last part of the hostname after the last dot is at least two characters long
+    const lastDotIndex = hostname.lastIndexOf('.');
+    const lastPart = hostname.slice(lastDotIndex + 1);
+    if (lastPart.length < 2) {
+      return false;
+    }
   
-  export default validateUrl;
-  
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default validateUrl;
